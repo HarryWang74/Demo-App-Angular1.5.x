de82e79c9b0e7cfa86f2576046974b6f5ee4a43e
Harry <harry74323@gmail.com>
2017-09-05 16:59:50 +1000

    step12

--- products/rating.js
+++ products/rating.js
@@ -1,11 +1,29 @@
 angular.module('myApp').component('rating', {
     templateUrl: "/products/rating.html",
     controllerAs: "ctrl",
+    bindings:{
+        rating: '<'
+    },      
     controller: [function(){
         var ctrl = this;
-
+        ctrl.maxRating = 5;
+        ctrl.rating = 2;
         ctrl.$onInit = function () {
 
         };
+        ctrl.$onChanges = function (changes) {
+            console.log(changes);
+            if (typeof (changes.rating) != "undefined" && typeof (changes.rating.currentValue) != "undefined") {
+                ctrl.rating = changes.rating.currentValue;
+            }
+        }
+        
+        ctrl.getMaxRating = function(){
+            return new Array(ctrl.maxRating);
+        }
+
+        ctrl.changeRating = function(index){
+            ctrl.rating = index + 1;
+        }
     }],
   });
