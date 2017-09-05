d45ea22db1bca31fc3d74d09935978441c763450
Harry <harry74323@gmail.com>
2017-09-05 11:51:44 +1000

    step11

---
+++ products/rating.js
@@ -0,0 +1,11 @@
+angular.module('myApp').component('rating', {
+    templateUrl: "/products/rating.html",
+    controllerAs: "ctrl",
+    controller: [function(){
+        var ctrl = this;
+
+        ctrl.$onInit = function () {
+
+        };
+    }],
+  });
